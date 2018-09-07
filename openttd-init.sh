f3924cb2ece5c0424753a6bd59058fe884cc7005
Quinn Brown <PRTZLnet@users.noreply.github.com>
2018-09-07 10:11:40 -0400

    Still broken
    half works, certain things don't like to be invoked through a bash script

--- openttd-init.sh
+++ openttd-init.sh
@@ -5,21 +5,21 @@ outpt = $(getconf _NPROCESSORS_ONLN)
 
 echo "this step will depend on the internet connection to the server"
 echo "[......]"
-wget https://binaries.openttd.org/releases/1.8.0/openttd-1.8.0-source.tar.gz > /dev/null
+wget https://binaries.openttd.org/releases/1.8.0/openttd-1.8.0-source.tar.gz > /dev/null 2>&1
 echo "[=.....]"
-tar xzf openttd-1.8.0-source.tar.gz > /dev/null
+tar xzf openttd-1.8.0-source.tar.gz > /dev/null 2>&1
 echo "[==....]"
-sudo apt-get install build-essential pkg-config libsdl1.2-dev subversion patch zlib1g-dev liblzo2-dev liblzma-dev libfontconfig-dev libicu-dev > /dev/null
+yes | sudo apt-get install build-essential pkg-config libsdl1.2-dev subversion patch zlib1g-dev liblzo2-dev liblzma-dev libfontconfig-dev libicu-dev > /dev/null 2>&1
 echo "[===....]"
 pushd openttd-1.8.0/
-./configure --enable-dedicated > /dev/null
+./configure --enable-dedicated > /dev/null 2>&1
 echo "[====...]"
 echo "this step may take as long as 20 minutes, please be patient. If it fails, you'll know."
-make > /dev/null 
-sudo make install > /dev/null
+make --jobs=1 > /dev/null 2>&1
+sudo make install > /dev/null 2>&1
 echo "[=====..]"
-wget http://binaries.openttd.org/bananas/basegraphic/zBase-v5588.tar.gz > /dev/null 
-gunzip zBase-v5588.tar.gz > /dev/null
+wget http://binaries.openttd.org/bananas/basegraphic/zBase-v5588.tar.gz > /dev/null 2>&1
+gunzip zBase-v5588.tar.gz > /dev/null 2>&1
 mkdir -p .openttd/baseset
 mv zBase-v5588.tar .openttd/baseset
 echo "[======.]"
